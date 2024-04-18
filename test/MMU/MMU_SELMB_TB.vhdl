f20a72e377056fd8c69a6c3e870cefcf39856ec9
frozen-signal <132361725+frozen-signal@users.noreply.github.com>
2024-04-18 12:51:44 -0400

    Fixed /EN80 falling when it's not supposed to (#42)
    Re-wrote SELMB_N and tests
    Added enabling to MC05X_N: forced HIGH during PHI_1

--- test/MMU/MMU_SELMB_TB.vhdl
+++ test/MMU/MMU_SELMB_TB.vhdl
@@ -7,115 +7,168 @@ end MMU_SELMB_TB;
 architecture MMU_SELMB_TEST of MMU_SELMB_TB is
     component MMU_SELMB is
         port (
-            A15, A14,
-            A13, A10    : in std_logic;
+            S_00_1XX    : in std_logic;  -- HIGH when the ADDRESS at the MMU's input is between $0000 - $01FF ($0[0-1]XX)
+            S_04_7XX    : in std_logic;  -- HIGH when the ADDRESS at the MMU's input is between $0400 - $07FF ($0[4-7]XX)
+            S_2_3XXX    : in std_logic;  -- HIGH when the ADDRESS at the MMU's input is between $2000 - $3FFF ($[2-3]XXX)
+            D_FXXX      : in std_logic;
             HIRES       : in std_logic;
-            PHI_0_7XX   : in std_logic;
             EN80VID     : in std_logic;
             PG2         : in std_logic;
             FLG1        : in std_logic;
             FLG2        : in std_logic;
             R_W_N       : in std_logic;
             ALTSTKZP    : in std_logic;
-            D_FXXX      : in std_logic;
-            PHI_0_1XX_N : in std_logic;
 
             SELMB_N : out std_logic
         );
     end component;
 
-    signal A15, A14, A13, A10 : std_logic;
-    signal HIRES              : std_logic;
-    signal PHI_0_7XX          : std_logic;
-    signal EN80VID            : std_logic;
-    signal PG2                : std_logic;
-    signal FLG1               : std_logic;
-    signal FLG2               : std_logic;
-    signal R_W_N              : std_logic;
-    signal ALTSTKZP           : std_logic;
-    signal D_FXXX             : std_logic;
-    signal PHI_0_1XX_N        : std_logic;
-    signal SELMB_N            : std_logic;
+    signal S_00_1XX : std_logic;
+    signal S_04_7XX : std_logic;
+    signal S_2_3XXX : std_logic;
+    signal D_FXXX : std_logic;
+    signal HIRES : std_logic;
+    signal EN80VID : std_logic;
+    signal PG2 : std_logic;
+    signal FLG1 : std_logic;
+    signal FLG2 : std_logic;
+    signal R_W_N : std_logic;
+    signal ALTSTKZP : std_logic;
+    signal SELMB_N : std_logic;
 
 begin
     dut : MMU_SELMB port map(
-        A15         => A15,
-        A14         => A14,
-        A13         => A13,
-        A10         => A10,
-        HIRES       => HIRES,
-        PHI_0_7XX   => PHI_0_7XX,
-        EN80VID     => EN80VID,
-        PG2         => PG2,
-        FLG1        => FLG1,
-        FLG2        => FLG2,
-        R_W_N       => R_W_N,
-        ALTSTKZP    => ALTSTKZP,
-        D_FXXX      => D_FXXX,
-        PHI_0_1XX_N => PHI_0_1XX_N,
-        SELMB_N     => SELMB_N
+        S_00_1XX => S_00_1XX,
+        S_04_7XX => S_04_7XX,
+        S_2_3XXX => S_2_3XXX,
+        D_FXXX => D_FXXX,
+        HIRES => HIRES,
+        EN80VID => EN80VID,
+        PG2 => PG2,
+        FLG1 => FLG1,
+        FLG2 => FLG2,
+        R_W_N => R_W_N,
+        ALTSTKZP => ALTSTKZP,
+        SELMB_N => SELMB_N
     );
 
     process begin
-        A15         <= '0';
-        A14         <= '0';
-        A13         <= '0';
-        A10         <= '0';
-        HIRES       <= '0';
-        PHI_0_7XX   <= '0';
-        EN80VID     <= '0';
-        PG2         <= '0';
-        FLG1        <= '0';
-        FLG2        <= '0';
-        R_W_N       <= '0';
-        ALTSTKZP    <= '0';
-        D_FXXX      <= '0';
-        PHI_0_1XX_N <= '0';
+
+        -- When accessing primary display range ($0400-$07FF) while EN80VID is set, the state of PG2 is set to SELMB_N
+        EN80VID  <= '1';
+        S_04_7XX <= '1';
+        PG2 <= '0';
         wait for 1 ns;
-        assert(SELMB_N = '0') report "expect SELMB_N LOW" severity error;
+        assert(SELMB_N = '0') report "Expected SELMB_N to be LOW" severity error;
+
+        PG2 <= '1';
+        wait for 1 ns;
+        assert(SELMB_N = '1') report "Expected SELMB_N to be HIGH" severity error;
+
+        EN80VID  <= '0';
+        R_W_N    <= '0';
+        FLG2     <= 'X';
+        wait for 1 ns;
+        assert(SELMB_N = 'X') report "This address range should be ignored when EN80VID is reset" severity error;
+
+
+        FLG2     <= 'U';
+        R_W_N    <= 'U';
+        S_04_7XX <= '0';
+        wait for 1 ns;
+
+
+        -- When accessing HIRES display range ($2000-$3FFF) while EN80VID and HIRES are set, the state of PG2 is set to SELMB_N
+        EN80VID  <= '1';
+        HIRES    <= '1';
+        S_2_3XXX <= '1';
+        PG2 <= '0';
+        wait for 1 ns;
+        assert(SELMB_N = '0') report "Expected SELMB_N to be LOW" severity error;
+
+        PG2 <= '1';
+        wait for 1 ns;
+        assert(SELMB_N = '1') report "Expected SELMB_N to be HIGH" severity error;
+
+        EN80VID  <= '0';
+        R_W_N    <= '0';
+        FLG2     <= 'X';
+        wait for 1 ns;
+        assert(SELMB_N = 'X') report "This address range should be ignored when EN80VID is reset" severity error;
+
+        EN80VID  <= '1';
+        HIRES    <= '0';
+        R_W_N    <= '0';
+        FLG2     <= 'X';
+        wait for 1 ns;
+        assert(SELMB_N = 'X') report "This address range should be ignored when HIRES is reset" severity error;
+
+
+        S_2_3XXX <= '0';
+        EN80VID  <= 'U';
+        HIRES    <= 'U';
+        R_W_N    <= 'U';
+        FLG2     <= 'X';
+        PG2 <= 'U';
+        wait for 1 ns;
+
+
+        -- When accessing the $0000-$01FF memory range, the state of ALTSTKZP is set to SELMB_N
+        S_00_1XX <= '1';
+        ALTSTKZP <= '0';
+        wait for 1 ns;
+        assert(SELMB_N = '0') report "Expected SELMB_N to be LOW" severity error;
 
         ALTSTKZP <= '1';
         wait for 1 ns;
-        assert(SELMB_N = '1') report "expect SELMB_N HIGH" severity error;
+        assert(SELMB_N = '1') report "Expected SELMB_N to be HIGH" severity error;
 
-        ALTSTKZP    <= '0';
-        PHI_0_1XX_N <= '1';
-        wait for 1 ns;
-        assert(SELMB_N = '0') report "expect SELMB_N LOW" severity error;
 
-        FLG2 <= '1';
+        S_00_1XX <= '0';
+        ALTSTKZP <= 'U';
         wait for 1 ns;
-        assert(SELMB_N = '1') report "expect SELMB_N HIGH" severity error;
 
-        FLG1  <= '1';
-        FLG2  <= '0';
-        R_W_N <= '1';
-        wait for 1 ns;
-        assert(SELMB_N = '1') report "expect SELMB_N HIGH" severity error;
 
-        FLG1      <= '0';
-        FLG2      <= '0';
-        R_W_N     <= '0';
-        A10       <= '1';
-        PHI_0_7XX <= '1';
+        -- When accessing the $D000-$FFFF memory range, the state of ALTSTKZP is set to SELMB_N
+        D_FXXX <= '1';
+        ALTSTKZP <= '0';
         wait for 1 ns;
-        assert(SELMB_N = '0') report "expect SELMB_N LOW" severity error;
+        assert(SELMB_N = '0') report "Expected SELMB_N to be LOW" severity error;
 
-        EN80VID <= '1';
-        PG2     <= '1';
+        ALTSTKZP <= '1';
         wait for 1 ns;
-        assert(SELMB_N = '1') report "expect SELMB_N HIGH" severity error;
+        assert(SELMB_N = '1') report "Expected SELMB_N to be HIGH" severity error;
 
-        A10       <= '0';
-        PHI_0_7XX <= '0';
-        A15       <= '0';
-        A13       <= '1';
-        wait for 1 ns;
-        assert(SELMB_N = '0') report "expect SELMB_N LOW" severity error;
 
-        HIRES <= '1';
+        D_FXXX <= '0';
+        ALTSTKZP <= 'U';
         wait for 1 ns;
-        assert(SELMB_N = '1') report "expect SELMB_N HIGH" severity error;
+
+        -- If it's a read operation, the state of FLG1 is set to SELMB_N
+        R_W_N    <= '1';
+        FLG1     <= '0';
+        wait for 1 ns;
+        assert(SELMB_N = '0') report "Expected SELMB_N to be LOW" severity error;
+
+        FLG1     <= '1';
+        wait for 1 ns;
+        assert(SELMB_N = '1') report "Expected SELMB_N to be HIGH" severity error;
+
+
+        FLG1     <= 'U';
+
+
+        -- In any other cases, the state of FLG2 is set to SELMB_N
+        R_W_N    <= '0';
+        FLG2     <= '0';
+        wait for 1 ns;
+        assert(SELMB_N = '0') report "Expected SELMB_N to be LOW" severity error;
+
+        FLG2     <= '1';
+        wait for 1 ns;
+        assert(SELMB_N = '1') report "Expected SELMB_N to be HIGH" severity error;
+
+
 
         assert false report "Test done." severity note;
         wait;
